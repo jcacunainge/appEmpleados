d71c2d5cdf4e3b9897e5a9f8a6184b5026277d9c
Jhan Carlos Acuña Ordoñez <102251412+jcacunainge@users.noreply.github.com>
2023-02-17 13:30:39 -0500

    actualizar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Modulos
 import { AppRoutingModule } from './app-routing.module';
+
+//Componentes
 import { AppComponent } from './app.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
+import { CreateEmpleadosComponent } from './components/create-empleados/create-empleados.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmpleadosComponent,
+    CreateEmpleadosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/create-empleados/create-empleados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-empleados',
+  templateUrl: './create-empleados.component.html',
+  styleUrls: ['./create-empleados.component.css']
+})
+export class CreateEmpleadosComponent {
+
+}
