cdf52465836dc04c98c40e17da347f9432bd447f
Jhan Carlos Acuña Ordoñez <102251412+jcacunainge@users.noreply.github.com>
2023-02-19 16:18:51 -0500

    actualizar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { CreateEmpleadosComponent } from './components/create-empleados/create-e
 import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 
 const routes: Routes = [
+  //Si no se agrega nada a la ruta, redirije a el componente list-em
   {path: '', redirectTo: 'list-empleados', pathMatch: 'full'},
   {path: 'list-empleados', component: ListEmpleadosComponent},
   {path: 'create-empleado', component: CreateEmpleadosComponent},
+  //Redireciona al componente principal, cuando se utiliza una ruta inexistente
   {path: '**', redirectTo: 'list-empleados', pathMatch: 'full'}
 
 ];
